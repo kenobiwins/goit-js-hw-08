7c1a3b7fc2ee1078bfa16dde537f882f8fd2038a
Max Sidorenko <fivemoshr@gmail.com>
2022-10-14 19:57:48 +0300

    add2task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import { storage } from './storageMethods';
+import throttle from 'lodash.throttle';
 
+const playerId = document.getElementById('vimeo-player');
+const player = new Player(playerId);
+const CURRENT_VIDEO_TIME = "videoplayer-current-time";
+
+player.on('timeupdate',throttle(currentTimeOnVideo,1000))
+player.setCurrentTime(storage.readItem(CURRENT_VIDEO_TIME))
+
+function currentTimeOnVideo(e) {
+    storage.updateItem(CURRENT_VIDEO_TIME, e.seconds)
+}
+
+    // if (e.seconds == e.duration) {
+    //     storage.deleteItem(CURRENT_VIDEO_TIME)
+    // }
---
+++ src/js/storageMethods.js
@@ -0,0 +1,19 @@
+export const storage = {
+    readItem(key,value){
+    try {
+   return JSON.parse(localStorage.getItem(key))    
+    } catch (error) {
+    console.error(error.name);
+    }
+},
+    updateItem(key, value) {
+    try { 
+    return storage.readItem === null ? undefined : localStorage.setItem(key,JSON.stringify(value))
+    } catch (error) {
+    console.error(error.massage);
+    }
+},
+    deleteItem(key) {
+        return localStorage.removeItem(key);    
+    },
+};
