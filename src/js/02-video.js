69e5fb11558b8342840a2adad61b5b2d1c51018e
Max Sidorenko <fivemoshr@gmail.com>
2022-10-16 20:32:18 +0300

    add-tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,15 +4,16 @@ import throttle from 'lodash.throttle';
 
 const playerId = document.getElementById('vimeo-player');
 const player = new Player(playerId);
-const CURRENT_VIDEO_TIME = "videoplayer-current-time";
 
-player.on('timeupdate',throttle(currentTimeOnVideo,1000))
-player.setCurrentTime(storage.readItem(CURRENT_VIDEO_TIME))
+const CURRENT_VIDEO_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(currentTimeOnVideo, 1000));
+player.setCurrentTime(storage.readItem(CURRENT_VIDEO_TIME));
 
 function currentTimeOnVideo(e) {
-    storage.updateItem(CURRENT_VIDEO_TIME, e.seconds)
+  if (e.seconds === e.duration) {
+    storage.updateItem(CURRENT_VIDEO_TIME, (e.seconds = 0));
+  } else {
+    storage.updateItem(CURRENT_VIDEO_TIME, e.seconds);
+  }
 }
-
-    // if (e.seconds == e.duration) {
-    //     storage.deleteItem(CURRENT_VIDEO_TIME)
-    // }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+import { storage } from './storageMethods';
 
+const refForm = document.querySelector('.feedback-form');
+
+const { email, message } = refForm;
+const KEY_FOR_FORM = 'feedback-form-state';
+const formStorage = {};
+
+refForm.addEventListener('input', throttle(eventOnTextArea, 500));
+refForm.addEventListener('submit', sumbitForm);
+addEventListener('DOMContentLoaded', readLocalStorage);
+
+function readLocalStorage(e) {
+  const savedProperty = storage.readItem(KEY_FOR_FORM);
+  if (savedProperty) {
+    email.value = savedProperty.email;
+    message.value = savedProperty.message;
+  }
+}
+
+function eventOnTextArea(e) {
+  const { name, value } = e.target;
+  formStorage[name] = value;
+  storage.updateItem(KEY_FOR_FORM, formStorage);
+}
+
+function sumbitForm(e) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  formData.forEach((value, name) => {
+    console.log(`${name} : ${value}`);
+  });
+  e.currentTarget.reset();
+  storage.deleteItem(KEY_FOR_FORM);
+}
